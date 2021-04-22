1526b0d3ac9375bcdc72eec14a1a6411d669db64
Andy Bowskill <AndyBowskill@users.noreply.github.com>
2021-04-22 11:19:17 +0100

    Add some documentation.

--- main.py
+++ main.py
@@ -1,43 +1,19 @@
-class MakeSlug(object):
-    ''' Slug value (a part of a URL) from a regular sentence. '''
-
-    def check_for_errors(self, sentence):
-
-        error = True
-        error_text = None
-
-        self.sentence = sentence
-
-        if self.sentence:
-            error = False
-        else:
-            error_text = "The sentence must be populated. Please try again."
-
-        return error, error_text
-
-    def generate(self):
-
-        slug = self.sentence.lower()
-        slug = slug.replace(" ", "-")
-
-        return slug
+import makeslug
 
 
 def main():
 
     print("Make a slug from sentence you provide.")
 
-    make_slug = MakeSlug()
-
     error = True
 
     while error:
         sentence = input("Please enter one sentence: ")
-        error, error_text = make_slug.check_for_errors(sentence)
+        error, error_text = makeslug.check_for_errors(sentence)
         if error:
             print(error_text)
 
-    slug = make_slug.generate()
+    slug = makeslug.generate()
     print(f"The generated slug is: {slug}")
 
 
---
+++ makeslug.py
@@ -0,0 +1,35 @@
+def check_for_errors(sentence: str) -> tuple[bool, str]:
+    """Check for errors on the sentence.
+
+    Args:
+        sentence: The sentence to check.
+
+    Returns:
+        error bool and error text message.
+    """
+
+    error = True
+    error_text = None
+
+    if sentence:
+        error = False
+    else:
+        error_text = "The sentence must be populated. Please try again."
+
+    return error, error_text
+
+
+def generate(sentence: str) -> str:
+    """Generate a slug from a sentence.
+
+    Args:
+        sentence: The sentence to check.
+
+    Returns:
+        generated slug value.
+    """
+
+    slug = sentence.lower()
+    slug = slug.replace(" ", "-")
+
+    return slug
